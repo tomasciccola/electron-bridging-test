e3f1308b916392a8a1d445a5afe4e3668fff9289
Tomás Ciccola <tciccola@digital-democracy.com>
2023-06-15 11:31:33 -0300

    feat: rpc-reflector + messagePorts

--- src/main.js
+++ src/main.js
@@ -14,12 +14,20 @@ app.whenReady().then(async () => {
     show: false,
     webPreferences: {
       sandbox: false,
-      preload: path.join(__dirname, 'preload.js')
+      preload: path.join(__dirname, 'preload.js'),
     }
   })
   await worker.loadFile(path.join(__dirname, 'worker.html'))
 
-  const { port1, port2 } = new MessageChannelMain()
-  mainWindow.webContents.postMessage('port', null, [port1])
-  worker.webContents.postMessage('port', null, [port2])
+  const sendPorts = () => {
+    const { port1, port2 } = new MessageChannelMain()
+    mainWindow.webContents.postMessage('port-renderer', null, [port1])
+    worker.webContents.postMessage('port-worker', null, [port2])
+  }
+  sendPorts()
+  mainWindow.webContents.on('did-finish-load', () => {
+    console.log('main reloaded, reloading worker')
+    worker.webContents.reload()
+    sendPorts()
+  })
 })
--- src/preload.js
+++ src/preload.js
@@ -1,9 +1,30 @@
-const { ipcRenderer } = require('electron')
-const windowLoaded = new Promise(resolve => {
+const { ipcRenderer, contextBridge } = require('electron')
+const { createServer, createClient } = require('rpc-reflector')
+const windowLoaded = new Promise((resolve) => {
   window.onload = resolve
 })
 
-ipcRenderer.on('port', async (e) => {
+const api = {
+  /** @param {String} who */
+  greet: (who) => `hi ${who}!`,
+  greetAsync: (who) => new Promise((res, rej) => {
+    setTimeout(() => res(`hi ${who}!`), 1000)
+  })
+}
+
+ipcRenderer.on('port-renderer', async (e) => {
   await windowLoaded
+  contextBridge.exposeInMainWorld('createClient', createClient)
+  window.postMessage('port', '*', e.ports)
+  console.log('client', createClient(e.ports[0]))
+})
+
+ipcRenderer.on('port-worker', async (e) => {
+  await windowLoaded
+  const [msgChannel] = e.ports
+  const clientApi = createServer(api, msgChannel)
+  console.log('api', clientApi)
+  // contextBridge.exposeInMainWorld('createServer', createServer)
+  // contextBridge.exposeInMainWorld('api', api)
   window.postMessage('port', '*', e.ports)
 })
